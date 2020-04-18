ce610c5febaed0218c230e5281683690cbc1b571
anebz <anebzt@protonmail.com>
2020-04-18 11:57:10 +0200

    bpe_word_align

--- README.md
+++ README.md
@@ -4,27 +4,12 @@ Master thesis repo
 
 ## tasks
 
-* [X] Check that . can be present in merge list
-* [X] b ut w e -> [0, 0, 1, 1] to which word does each bpe belong to. do with both langs
-* [ ] get bpe alignments
-* [ ] bpe alignment -> word alignment
-  * 1 in the bpe list belongs to word nº0
+* [X] get bpe alignments
+* [X] bpe alignment -> word alignment
 * [ ] do calc_score with gold from data
-* [ ] check calc_score for normal input, what's the good bpe size, 2k, 5k?
+* [Linux] check calc_score for normal input
+* [Linux] Get different bpe sized outputs, 2k, 5k?
 * [ ] figure, bpe size vs. alignment quality / score
-
-```python
-BPE to word mapping example:
-_B ut _this _is _not _wh at _ha pp en s
-e_bpe_map: [0, 0, 1, 2, 3, 4, 4, 5, 5, 5, 5]
-
-bpe_aligns: 1-2 4-5 4-6
-word_aligns = [(e_bpe_map[p[0]], d_bpe_map[p[1]]) for p in bpe_aligns]
-
-word_aligns: 0-1
-
-d_bpe_map: [0, 0, 1, 2, 3, 4, 4, 5, 5, 6, 7, 7, 7, 8]
-```
 
 ## resources
 
--- subword_word.py
+++ subword_word.py
@@ -3,7 +3,7 @@ import glob
 import codecs
 import inspect
 
-def map_subword_to_word(corpus):
+def map_subword_to_word(corpus, bpes, lang):
     '''
     Input: list of sentences with subword separation
         [
@@ -11,14 +11,27 @@ def map_subword_to_word(corpus):
             '_Thi s _is _a _sent ence _.',
             ...
         ]
-    Output: list of vectors pointing to which word each subword belongs to
-        [
-            [0, 1, 2, 2, 3, 3, 3, 4],
-            [0, 0, 1, 2, 3, 4, 5],
-            ...
-        ]
+    Output: dictionary of each language and 
+    a list of vectors pointing to which word each subword belongs to
+        {
+            'eng':
+            [
+                [0, 1, 2, 2, 3, 3, 3, 4],
+                [0, 0, 1, 2, 3, 4, 5],
+                ...
+            ],
+            'deu':
+            [
+                [0, 1, 2, 3, 3, 4, 4, 5],
+                [0, 0, 1, 1, 2, 3, 4],
+                ...
+            ],
+        }
+        
     '''
-    worded_corpus = []
+    if lang in bpes:
+        return bpes
+    bpes[lang] = []
     for sent in corpus:
         sent = sent.split()
         subwords = [0]
@@ -27,16 +40,46 @@ def map_subword_to_word(corpus):
             if subw[0] == u'\u2581':
                 i += 1
             subwords.append(i)
-        worded_corpus.append(subwords)
-    return worded_corpus
+        bpes[lang].append(subwords)
+    return bpes
+
+
+def bpe_word_align(bpes, bpe_aligns):
+    '''
+    Input: dictionary of bpes obtained as output of map_subword_to_word()
+    Output: list of word alignments
+        [
+            '0-0 0-1 1-1 1-2 3-1 2-4',
+            ...
+        ]
+    '''
+    all_word_aligns = []
+    # iterate all sentences
+    for sent1, sent2, bpe_al in zip(bpes['eng'], bpes['deu'], bpe_aligns):
+        word_aligns = ''
+        # iterate each alignment
+        # bpe_al.split('\t')[1] to remove the index in the alignment file .gdfa
+        for al in bpe_al.split('\t')[1].split():
+            new_al = str(sent1[int(al[0])]) + '-' + str(sent2[int(al[-1])])
+            # skip already seen word alignments
+            if not new_al in word_aligns:
+                word_aligns += new_al + ' '
+        all_word_aligns.append(word_aligns[:-1])
+    return all_word_aligns
 
 
 if __name__ == "__main__":
     currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
     datapath = os.path.join(currentdir, 'data')
 
-    os.chdir(datapath)
-    for ifile in glob.glob("*.bpe"):
+    num_symbols = '1000'
 
-        argsinput = codecs.open(ifile, encoding='utf-8').readlines()
-        worded_corpus = map_subword_to_word(argsinput)
+    os.chdir(datapath)
+    bpes = {}
+    for ifile in glob.glob("*_"+num_symbols+".bpe"):
+        lang, _ = ifile.split('.')[0].split('_')
+        argsinput = codecs.open(ifile, encoding='utf-8')
+        bpes = map_subword_to_word(argsinput, bpes, lang)
+
+    argsalign = codecs.open(os.path.join(datapath, 'fastalign', num_symbols+'.gdfa'), encoding='utf-8')
+    all_word_aligns = bpe_word_align(bpes, argsalign)
