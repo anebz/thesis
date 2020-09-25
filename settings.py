560b883bc659bfa95a7edee39c31830c65891f7b
anebz <anebzt@protonmail.com>
2020-09-25 11:57:38 +0200

    modified presentation (presentation.pdf) and reordering of hyperparameters

--- settings.py
+++ settings.py
@@ -5,15 +5,17 @@ from os.path import join
 
 word_sep = u'\u2581' # symbol to use for word separators
 source, target = 'eng', 'deu' # eng, deu, ron, hin
-source_bpe, target_bpe = False, False # Set True to source to make BPE only on source mode, same in Target. both can't be true at the same time
+mode = "fastalign"  # fastalign, eflomal
+source_bpe, target_bpe = False, True # Set True to source to make BPE only on source mode, same in Target. both can't be true at the same time
 
-space = True # True for space mode, False for no space mode
-scoring = True
-learn_merges = 10000 # how many BPE units to learn in learn_bpe.py
-merges = [2000] # create segmentations with different number of merges
+space = False # True for space mode, False for no space mode
+scoring = False # True to activate the scoring method
 dropout = 0.1 # dropout rate
+
+learn_merges = 10000 # how many BPE units to learn in learn_bpe.py
+merges = [200, 500] # create segmentations with different number of merges
 dropout_samples = 10 # how many samples to create in dropout mode
-merge_threshold = [0.7] # alignment threshold for dropout mode
+merge_threshold = [0.5, 0.7] # alignment threshold for dropout mode
 
 # paths for input files
 rootdir = os.getcwd()
@@ -31,7 +33,6 @@ baselinedir = join(rootdir, 'reports', 'scores_normal_bpe')
 scoredir = join(rootdir, 'reports', 'scores_' + ('dropout_bpe' if dropout > 0 else 'normal_bpe'))
 
 # paths for alignment algorithms
-mode = "eflomal" #fastalign, eflomal
 fastalign_path = join(rootdir, "tools/fast_align/build/fast_align")
 atools_path = join(rootdir, "tools/fast_align/build/atools")
 eflomal_path = join(rootdir, "tools/eflomal")
